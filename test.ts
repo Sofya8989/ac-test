6e46efa6f7ffd81b553c1508dc2902000688248c
Sofya Ospanova <52817218+Sofya8989@users.noreply.github.com>
2022-09-04 11:37:33 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,33 +3,39 @@ import { assertEquals } from "testing/asserts.ts";
 
 const CHROME_BIN = Deno.env.get("CHROME_BIN");
 
-Deno.test("E2E test", async (t) => {
+Deno.test("E2E test", async () => {
   /* Start Sinco */
   const { browser, page } = await buildFor("chrome", {
     binaryPath: CHROME_BIN,
   });
+  await page.location("https://www.active-connector.com/"); // Go to this page
 
-  const index = "http://localhost:8000/";
-
-  /* Beginning of tests */
-
-  await t.step("click the logo", async () => {
-    await page.location(index);
-
+  // Try here so if the assertion fails, we can close all resources
+  try {
+    assertEquals(await page.location(), "https://www.active-connector.com/");
+  } catch (e) {
+    await browser.close();
+    throw e;
+  }
+  const index = await page.querySelector(
+    'a[href="http://localhost:8000/]',
+    );
+ 
     const image = await page.querySelector("img");
     await image.click({ waitFor: "navigation" });
+    const location (any) = await page.location(); // Get all data before we close, then we can safely assert
+    // Once finished, close to clean up any processes
 
     assertEquals(await page.location(), "https://www.active-connector.com/");
   });
 
   await page.location(index);
 
-  await t.step("input is empty", async () => {
+ 
     const input = await page.querySelector("input");
     assertEquals(await input.value(), "");
   });
 
-  await t.step("error is not shown", async () => {
     const error = await page.evaluate(() =>
       document.querySelector("p")?.innerText
     );
@@ -46,7 +52,7 @@ Deno.test("E2E test", async (t) => {
     assertEquals(error, "error: empty input");
   });
 
-  await t.step("input a random string and click the button", async () => {
+
     const input = await page.querySelector("input");
 
     const name = crypto.randomUUID().slice(0, 7);
@@ -80,7 +86,9 @@ Deno.test("E2E test", async (t) => {
     assertEquals(body, `Job "engineer" is open for you!`);
   });
 
-  /* End of tests */
+ // Get all data before we close, then we can safely assert
+  // Once finished, close to clean up any processes
 
   await browser.close();
+  assertEquals(location, "http://localhost:8000/");
 });
